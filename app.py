55c883906d964417b719169d81ec4482a23d80e9
Hemaice <136627103+Hemaice@users.noreply.github.com>
2023-09-05 13:00:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,27 @@
+# app.py
 import streamlit as st
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
-from PIL import Image
+import tensorflow as tf
 import numpy as np
-st.title("Digit Recognizer")
 
-st.write("Predict the digit that is being represented in the image.")
+# Load your trained CNN model
+model = tf.keras.models.load_model('model.h5')
 
-model = load_model("model.h5")
-l=['0','1','2','3','4','5','6','7','8','9']
+st.title('Digit Recognition using CNN')
 
-model = load_model("model.h5")
+# Upload an image for prediction
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
-uploaded_file = st.file_uploader(
-    "Upload an image of a digit :", type="jpg"
-)
-predictions=-1
-if uploaded_file is not None:
-    image1 = Image.open(uploaded_file)
-    image1=image.smart_resize(image1,(28,28))
-    img_array = image.img_to_array(image1)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array/255.0
-    predictions = model.predict(img_array)
-    label=l[np.argmax(predictions)]
+if uploaded_image is not None:
+    # Preprocess the uploaded image
+    image = tf.image.decode_image(uploaded_image.read(), channels=1)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize(image, (28, 28))
+    image = np.expand_dims(image, axis=0)
 
+    # Perform inference
+    prediction = model.predict(image)
+    predicted_class = np.argmax(prediction)
 
-st.write("### Prediction Result")
-if st.button("Predict"):
-    if uploaded_file is not None:
-        image1 = Image.open(uploaded_file)
-        st.image(image1, caption="Uploaded Image", use_column_width=True)
-        st.markdown(
-            f"<h2 style='text-align: center;'>Image of {label}</h2>",
-            unsafe_allow_html=True,
-        )
-    else:
-        st.write("Please upload file or choose sample image.")
+    st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+    st.write(f'Predicted Digit: {predicted_class}')
 
-
-st.write("If you would not like to upload an image, you can use the sample image instead:")
-sample_img_choice = st.button("Use Sample Image")
-
-if sample_img_choice:
-    image1 = Image.open("seed_charlock.png")
-    image1=image.smart_resize(image1,(256,256))
-    img_array = image.img_to_array(image1)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array/255.0
-    predictions = model.predict(img_array)
-    label=l[np.argmax(predictions)]
-    st.markdown(
-        f"<h2 style='text-align: center;'>{label}</h2>",
-        unsafe_allow_html=True,
-    )
