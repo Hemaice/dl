41e4f1627fbc1c877abde7565ef58cf285dfe44c
Hemaice <136627103+Hemaice@users.noreply.github.com>
2023-09-05 13:14:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,27 @@
+# app.py
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-from PIL import Image
 
 # Load your trained CNN model
-model = tf.keras.models.load_model('C:/Users/Student/Desktop/dl ass/hema/model.h5')
+model = tf.keras.models.load_model('model.h5')
 
 st.title('Digit Recognition using CNN')
 
-# Sidebar for user options
-st.sidebar.title('Options')
-option = st.sidebar.selectbox('Choose an option:', ['Draw Digit', 'Upload Image'])
+# Upload an image for prediction
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
-if option == 'Draw Digit':
-    st.sidebar.text('Draw a digit and click "Predict"')
-    canvas = st.canvas(fill_color="#000000", stroke_width=10, stroke_color="#FFFFFF", background_color="#000000", height=150, width=150)
-    predict_button = st.sidebar.button("Predict")
+if uploaded_image is not None:
+    # Preprocess the uploaded image
+    image = tf.image.decode_image(uploaded_image.read(), channels=1)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize(image, (28, 28))
+    image = np.expand_dims(image, axis=0)
 
-    if predict_button:
-        # Convert the canvas drawing to an image
-        digit_image = canvas.image_data.astype(np.uint8)
+    # Perform inference
+    prediction = model.predict(image)
+    predicted_class = np.argmax(prediction)
 
-        # Resize the image to 28x28 pixels and preprocess it
-        pil_image = Image.fromarray(digit_image)
-        pil_image = pil_image.convert('L')  # Convert to grayscale
-        pil_image = pil_image.resize((28, 28))
-        digit_image = np.array(pil_image)
-        digit_image = digit_image / 255.0  # Normalize pixel values
+    st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+    st.write(f'Predicted Digit: {predicted_class}')
 
-        # Make a prediction
-        digit_image = np.expand_dims(digit_image, axis=0)
-        prediction = model.predict(digit_image)
-        predicted_class = np.argmax(prediction)
-
-        st.image(pil_image, caption='Drawn Digit', use_column_width=True)
-        st.write(f'Predicted Digit: {predicted_class}')
-
-elif option == 'Upload Image':
-    st.sidebar.text('Upload an image and click "Predict"')
-    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-
-    predict_button = st.sidebar.button("Predict")
-
-    if uploaded_image is not None and predict_button:
-        # Preprocess the uploaded image
-        image = Image.open(uploaded_image)
-        image = image.convert('L')  # Convert to grayscale
-        image = image.resize((28, 28))
-        image = np.array(image)
-        image = image / 255.0  # Normalize pixel values
-
-        # Make a prediction
-        image = np.expand_dims(image, axis=0)
-        prediction = model.predict(image)
-        predicted_class = np.argmax(prediction)
-
-        st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
-        st.write(f'Predicted Digit: {predicted_class}')
